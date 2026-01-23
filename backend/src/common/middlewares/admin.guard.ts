6b1cf2522c4f05783f583ba48adecf1f99b16721
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-01-23 14:10:16 +0530

    updated the backend after completion

--- backend/src/common/middlewares/admin.guard.ts
+++ backend/src/common/middlewares/admin.guard.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const AdminGhostGuard = (req: Request, res: Response, next: NextFunction) => {
-  // Check for the "Invisible Key" in headers
+
   const secretKey = req.headers['x-uplink-security'];
   
-  // If key is missing or wrong -> Fake 404 (Ghost Mode)
+
   if (secretKey !== process.env.ADMIN_SECRET_KEY) {
     return res.status(404).json({
       success: false,
-      message: "Route not found" // Lie to the attacker
+      message: "Route not found" 
     });
   }
   
-  next(); // Key matches, open the gate
+  next();
 };
