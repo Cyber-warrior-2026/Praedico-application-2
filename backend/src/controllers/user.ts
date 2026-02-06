c48bb8b7d1d182a74ac95e1a334264b315568085
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-02-06 23:58:47 +0530

    Fixed a problem in user.controller.ts file for deploying

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -131,14 +131,14 @@ export class UserController {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: "lax",
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: "lax",
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
